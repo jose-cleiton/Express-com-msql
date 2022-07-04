99645c2a81d8a769a672325c79bf8e43fc063e6f
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-07-03 21:34:46 -0300

    feat Adiconando rotas get e post para person

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 
+const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
-const app = express();
-
 const store = { data: [], index: 0 };
+
+/** ListenPerson */
+app.get('/person', (req, res) => {
+  const items = store.data;  
+  res.json(items);
+});
+ 
+/** addPerson */
+app.post('/person', (req, res) => {
+  const data = req.body;
+  store.index += 1;
+  const id = store.index;
+  const item = { ...data, id };
+  store.data.push(item);
+ return res.status(201).json(item);
+ });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
